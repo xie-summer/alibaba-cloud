db7b57f698b5d1b37db37892b080fcb763ad2989
xie-summer <xieshengrong@live.com>
2019-04-29 00:27:30 +0800

    1,update 自动注入

--- common-modules/framework-mvc/src/main/java/com/springframework/mvc/aop/LimitInterceptor.java
+++ common-modules/framework-mvc/src/main/java/com/springframework/mvc/aop/LimitInterceptor.java
@@ -10,6 +10,7 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
@@ -27,6 +28,7 @@ import java.lang.reflect.Method;
  */
 @Aspect
 @Configuration
+@ConditionalOnBean(value = RedisTemplate.class)
 public class LimitInterceptor {
 
     private static final Logger logger = LoggerFactory.getLogger(LimitInterceptor.class);
